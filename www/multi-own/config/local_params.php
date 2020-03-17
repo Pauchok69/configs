da34b3dd7e50053d461ff5cc1ddeae805e7caf5c
Adrian <adrian@shinningcreation.com>
2020-03-17 10:33:39 -0400

    Add multi-own configs

---
+++ www/multi-own/config/local_params.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'apilight' => [
+        'testing' => false,
+    ],
+    'api' => [
+        'testing' => true,
+    ],
+    'apihf' => [
+        'testing' => true,
+    ],
+    'email.send' => false,
+    'dev' => true,
+    'prod' => false,
+    'order.prefix' => 'TEST_TEST_',
+    'deprecated.warnings.off' => true,
+
+    'admin_password' => '1234',
+    'dev_password' => '1234',
+];
